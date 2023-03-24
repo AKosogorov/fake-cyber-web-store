79b0303883a717858e6b28e2897d842a9907dc85
Anton <kosogorov.anton@gmail.com>
2023-03-24 17:27:01 +0500

    lint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -53,6 +53,48 @@ module.exports = {
         alphabetical: false
       }
     ],
+    "vue/html-quotes": [ "error", "double", { "avoidEscape": false } ],
+    'vue/order-in-components': [
+      'error',
+      {
+        order: [
+          ['template', 'render'],
+          'el',
+          'name',
+          'key',
+          'parent',
+          'functional',
+          ['delimiters', 'comments'],
+          ['components', 'directives', 'filters'],
+          'extends',
+          'mixins',
+          ['provide', 'inject'],
+          'ROUTER_GUARDS',
+          'layout',
+          'middleware',
+          'validate',
+          'scrollToTop',
+          'transition',
+          'loading',
+          'inheritAttrs',
+          'model',
+          'emits',
+          ['props', 'propsData'],
+          'setup',
+          'asyncData',
+          'data',
+          'fetch',
+          'head',
+          'computed',
+          'watch',
+          'watchQuery',
+          'LIFECYCLE_HOOKS',
+          'methods',
+          'renderError'
+        ]
+      }
+
+    ],
     'vue/block-tag-newline': [
       'error',
       {
