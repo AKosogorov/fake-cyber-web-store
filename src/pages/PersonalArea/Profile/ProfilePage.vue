eccb05e32fd43bf864b94a0059213da3164e5f63
Anton <kosogorov.anton@gmail.com>
2023-04-17 21:40:38 +0500

    baseModal

--- src/pages/PersonalArea/Profile/ProfilePage.vue
+++ src/pages/PersonalArea/Profile/ProfilePage.vue
@@ -3,7 +3,7 @@
     <h1 class="mb-s">Profile</h1>
 
     <div class="column gap-xs">
-      <div class="row gap-s">
+      <div class="flex gap-s">
         <UserBadge :user="session.user" />
 
         <ButtonEdit />
@@ -12,7 +12,7 @@
       <div class="row gap-s">
         <VInfo label="Email" :txt="session.user.email" />
 
-        <ButtonEdit />
+        <ButtonEdit @click="toggle" />
       </div>
 
       <div class="row gap-s">
@@ -22,14 +22,22 @@
       </div>
     </div>
   </div>
+
+  <BaseModal v-if="isActive" @close="toggle">
+    <VInfo label="Email" :txt="session.user.email" />
+  </BaseModal>
 </template>
 
 <script setup lang="ts">
 import { UserBadge } from '@/entities/User'
 import { VInfo } from '@/shared/ui/text'
 import { ButtonEdit } from '@/shared/ui/buttons'
+import { BaseModal } from '@/shared/ui/modal'
 
 import { useSessionStore } from '@/entities/Session/model'
+import useToggle from '@/shared/lib/use/useToggle'
 
 const session = useSessionStore()
+
+const { isActive, toggle } = useToggle()
 </script>
---
+++ src/shared/ui/modal/base/BaseModal.vue
@@ -0,0 +1,36 @@
+<template>
+  <teleport to="body">
+    <div class="modal" :class="className">
+      <div class="modal__wrap" @click.self="close">
+        <div class="modal__content">
+          <ButtonCross class="modal__button-close" @click="close" />
+
+          <h3 v-if="title" class="modal__title center">{{ title }}</h3>
+
+          <slot />
+        </div>
+      </div>
+    </div>
+  </teleport>
+</template>
+
+<script setup lang="ts">
+import { ButtonCross } from '@/shared/ui/buttons'
+
+const emit = defineEmits(['close'])
+
+interface IProps {
+  title?: string
+  className?: string
+}
+
+defineProps<IProps>()
+
+function close() {
+  emit('close')
+}
+</script>
+
+<style lang="scss">
+@import 'styles';
+</style>
