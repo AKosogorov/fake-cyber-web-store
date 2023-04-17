1aea840c82d536f4fa09c9c5e39cb77dc7989a1c
Anton <kosogorov.anton@gmail.com>
2023-04-18 00:37:43 +0500

    entities: Cart api

--- src/entities/Cart/api/index.ts
+++ src/entities/Cart/api/index.ts
@@ -1,18 +1,50 @@
-import type { ICartProductBase, ICartResponse } from '../model/types'
-import type { AxiosPromise } from 'axios'
+import type { ICartFB, ICartProductBase, ICartResponse } from '../model/types'
+import {
+  createApiErrorGetById,
+  createApiErrorCreate,
+  FirebaseApi
+} from '@/shared/api'
 import { update } from '@/shared/api/requests'
 
-interface IApi {
-  calculate: (products: ICartProductBase[]) => AxiosPromise<ICartResponse>
+export const api = {
+  getById,
+  calculate,
+  create
 }
 
-const URL = 'carts'
+const name = 'basket'
 
-export const api: IApi = {
-  calculate: products => {
-    return update<ICartResponse>(URL, 1, {
+const errors = {
+  getById: createApiErrorGetById(name),
+  create: createApiErrorCreate(name),
+  calculate: 'Failed calculating the sum of the cost of the basket'
+} as const
+
+const CARTS_URL = 'carts'
+
+async function getById(id: FirebaseApi.TId) {
+  try {
+    return await FirebaseApi.getById<ICartFB>(CARTS_URL, id)
+  } catch (e) {
+    throw new Error(errors.getById)
+  }
+}
+
+async function create(data: ICartFB) {
+  try {
+    return await FirebaseApi.create(CARTS_URL, data)
+  } catch (e) {
+    throw new Error(errors.create)
+  }
+}
+
+async function calculate(products: ICartProductBase[]) {
+  try {
+    return await update<ICartResponse>(CARTS_URL, 1, {
       merge: false,
       products
     })
+  } catch (e) {
+    throw new Error(errors.calculate)
   }
 }
