70618d6151fb9ad9c655fc5fe72a5d8121de7b43
Anton <kosogorov.anton@gmail.com>
2023-04-17 19:27:58 +0500

    page: profile

--- src/pages/PersonalArea/Profile/ProfilePage.vue
+++ src/pages/PersonalArea/Profile/ProfilePage.vue
@@ -1,7 +1,22 @@
 <template>
   <div class="container">
     <h1 class="mb-s">Profile</h1>
+
+    <div class="column gap-xs">
+      <UserBadge :user="session.user" />
+
+      <VInfo label="Email" :txt="session.user.email" />
+
+      <VInfo label="Gender" :txt="session.user.gender" />
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { UserBadge } from '@/entities/User'
+import { VInfo } from '@/shared/ui/text'
+
+import { useSessionStore } from '@/entities/Session/model'
+
+const session = useSessionStore()
+</script>
